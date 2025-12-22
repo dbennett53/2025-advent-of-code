3fce5aae8d18f771925be8e2956c9be8df8f2489
Daniel Bennett <daniel.bennett53@gmail.com>
2025-12-21 16:13:58 -0800

    day 8 part 2 working

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -22,48 +22,64 @@ fn parse_file(filename: &str) -> Vec<Point> {
     out
 }
 
-fn find_closest_pairs(pairs: &Vec<Point>, num: usize) -> Vec<(usize, usize, i64)> {
-    let mut closest_pairs: Vec<(usize, usize, i64)> = vec![(0,0,i64::MAX); num + 1];
+fn find_closest_pairs(pairs: &Vec<Point>, num: i64) -> Vec<(usize, usize, i64)> {
+    // Return all pairs if number < 0
+    if num < 0 {
+        let mut closest_pairs: Vec<(usize, usize, i64)> = Vec::new();
+        for (i, p1) in pairs.iter().enumerate() {
+            for (j, p2) in pairs[i+1..].iter().enumerate() {
+                closest_pairs.push((i, j+i+1, dist(p1, p2)));
+            }
+        }
+        closest_pairs.sort_by_key(|x| x.2);
+        return closest_pairs;
+    } else {
+        let mut closest_pairs: Vec<(usize, usize, i64)> = vec![(0,0,i64::MAX); num as usize + 1];
 
-    for (i, p1) in pairs.into_iter().enumerate() {
-        for (j, p2) in pairs[i+1..].into_iter().enumerate() {
-            closest_pairs[num] = (i, j+i+1, dist(p1, p2));
-            closest_pairs.sort_by_key(|x| x.2);
+        for (i, p1) in pairs.into_iter().enumerate() {
+            for (j, p2) in pairs[i+1..].into_iter().enumerate() {
+                closest_pairs[num as usize] = (i, j+i+1, dist(p1, p2));
+                closest_pairs.sort_by_key(|x| x.2);
+            }
+        }
+
+        closest_pairs.pop();
+        closest_pairs
+    }
+}
+
+fn connect_circuit(circuits: &mut Vec<HashSet<usize>>, p1: usize, p2: usize) {
+    let sets_containing: Vec<usize> = circuits.iter()
+        .enumerate()
+        .filter(|(_, x)| x.contains(&p1) || x.contains(&p2))
+        .map(|(i, _)| i)
+        .rev()
+        .collect();
+    if sets_containing.is_empty() {
+        circuits.push(HashSet::from([p1, p2]));
+    } else {
+        for index in &sets_containing {
+            circuits[*index].insert(p1);
+            circuits[*index].insert(p2);
+        }
+        let first_index = sets_containing[sets_containing.len()-1];
+        for index in sets_containing[..sets_containing.len()-1].iter() {
+            let union: HashSet<usize> = circuits[first_index]
+                .union(&circuits[*index])
+                .map(|x| x.clone())
+                .collect();
+            circuits[first_index] = union;
+            circuits.remove(*index);
         }
     }
-
-    closest_pairs.pop();
-    closest_pairs
 }
 
 // Given a list of connections that have been made, determines the numbers and sizes of circuits
-fn get_circuits(connections: &Vec<(usize, usize, i64)>) -> Vec<HashSet<usize>> {
+fn connect_circuit_list(connections: &Vec<(usize, usize, i64)>) -> Vec<HashSet<usize>> {
     let mut circuits: Vec<HashSet<usize>> = vec![];
 
     for (c0, c1, _) in connections {
-        let sets_containing: Vec<usize> = circuits.iter()
-            .enumerate()
-            .filter(|(_, x)| x.contains(&c0) || x.contains(&c1))
-            .map(|(i, _)| i)
-            .rev()
-            .collect();
-        if sets_containing.is_empty() {
-            circuits.push(HashSet::from([*c0, *c1]));
-        } else {
-            for index in &sets_containing {
-                circuits[*index].insert(*c0);
-                circuits[*index].insert(*c1);
-            }
-            let first_index = sets_containing[sets_containing.len()-1];
-            for index in sets_containing[..sets_containing.len()-1].iter() {
-                let union: HashSet<usize> = circuits[first_index]
-                    .union(&circuits[*index])
-                    .map(|x| x.clone())
-                    .collect();
-                circuits[first_index] = union;
-                circuits.remove(*index);
-            }
-        }
+        connect_circuit(&mut circuits, *c0, *c1);
     }
 
     circuits.sort_by_key(|x| std::cmp::Reverse(x.len()));
@@ -71,16 +87,40 @@ fn get_circuits(connections: &Vec<(usize, usize, i64)>) -> Vec<HashSet<usize>> {
 }
 
 
+// Connects circuits until there are no unconnected boxes. Returns the indices of the last connection made
+fn connect_all_circuits(connections: &Vec<(usize, usize, i64)>, num_boxes: usize) -> (usize, usize) {
+    let mut circuits: Vec<HashSet<usize>> = (0..num_boxes)
+        .into_iter()
+        .map(|x| HashSet::from([x]))
+        .collect();
+
+    for (c0, c1, _) in connections {
+        connect_circuit(&mut circuits, *c0, *c1);
+        if circuits.len() == 1 {
+            return (*c0, *c1);
+        }
+    }
+    (0,0)
+}
+
+
 fn part_1(filename: &str, conn_num: usize, prod_num: i64) -> i64 {
     let boxes = parse_file(filename);
-    let connections = find_closest_pairs(&boxes, conn_num);
-    let circuits = get_circuits(&connections);
+    let connections = find_closest_pairs(&boxes, conn_num as i64);
+    let circuits = connect_circuit_list(&connections);
     let n = std::cmp::min(prod_num as usize, circuits.len());
     let product = circuits[0..n]
         .iter()
         .map(|x| x.len())
         .reduce(|prod, x| prod * x).unwrap() as i64;
     product
+}
+
+fn part_2(filename: &str) -> i64 {
+    let boxes = parse_file(filename);
+    let connections = find_closest_pairs(&boxes, -1);
+    let (a, b) = connect_all_circuits(&connections, boxes.len());
+    boxes[a].0 * boxes[b].0
 }
 
 #[cfg(test)]
@@ -102,12 +142,19 @@ mod tests {
     }
 
     #[test]
-    fn test_example() {
+    fn test_example1() {
         assert_eq!(part_1("inputs/example.txt", 10, 3), 40);
+    }
+
+    #[test]
+    fn test_example2() {
+        assert_eq!(part_2("inputs/example.txt"), 25272);
     }
 }
 
 fn main() {
     let part_1_solution = part_1("inputs/input.txt", 1000, 3);
+    let part_2_solution = part_2("inputs/input.txt");
     println!("Part 1: {}", part_1_solution);
+    println!("Part 2: {}", part_2_solution);
 }
